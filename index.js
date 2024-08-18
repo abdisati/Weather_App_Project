8cee625195ea701993757e109ba5b25bc96a99d0
Abdi Deresa <Abdishegute@gmail.com>
2024-08-18 16:06:29 -0700

    added functions to fetch and display

--- index.js
+++ index.js
@@ -6,16 +6,31 @@ const card = document.querySelector(".card");
 
 const apiKey = "c030ed5eefff362e384926114448ad97";
 
-weatherForm.addEventListener("sumbit", (event) => {
+weatherForm.addEventListener("submit", async (event) => {
   event.preventDefault();
   const city = cityInput.value;
   if (city) {
+    try {
+      const weatherData = await getWeatherData(city);
+      displayWeatherInfo(weatherData);
+    } catch (error) {
+      console.error(error);
+      displayError(error);
+    }
   } else {
     displayError("Please enter a city");
   }
 });
 
-async function getWeatherData(city) {}
+async function getWeatherData(city) {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+  const response = await fetch(apiUrl);
+  if (!response.ok) {
+    throw new Error("could not fetch weather data");
+  }
+  return await response.json();
+}
 
 function displayWeatherInfo(data) {}
 
@@ -27,4 +42,6 @@ function displayError(message) {
   errorDisplay.classList.add("errorDisplay");
 
   card.textContent = "";
+  card.style.display = "flex";
+  card.appendChild(errorDisplay);
 }
