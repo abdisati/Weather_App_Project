8fa1b7b1556e53bacc9f9b5622181618de4d20a4
Abdi Deresa <Abdishegute@gmail.com>
2024-08-18 16:54:32 -0700

    added an emoji functions

--- index.js
+++ index.js
@@ -32,9 +32,67 @@ async function getWeatherData(city) {
   return await response.json();
 }
 
-function displayWeatherInfo(data) {}
+function displayWeatherInfo(data) {
+  const {
+    name: city,
+    main: { temp, humidity },
+    weather: [{ description, id }],
+  } = data;
 
-function getWeatherEmoji(weatherId) {}
+  card.textContent = "";
+  card.style.display = "flex";
+  const cityDisplay = document.createElement("h1");
+  const tempDisplay = document.createElement("p");
+  const humidityDisplay = document.createElement("p");
+  const descDisplay = document.createElement("p");
+  const weatherEmoji = document.createElement("p");
+
+  cityDisplay.textContent = city;
+  tempDisplay.textContent = `${(temp - 273.15).toFixed(2)}°C`;
+  humidityDisplay.textContent = `Humidity: ${humidity}%`;
+  descDisplay.textContent = description;
+  weatherEmoji.textContent = getWeatherEmoji(id);
+
+  cityDisplay.classList.add("cityDisplay");
+  tempDisplay.classList.add("tempDisplay");
+  humidityDisplay.classList.add("humidityDisplay");
+  descDisplay.classList.add("descDisplay");
+  weatherEmoji.classList.add("weatherEmoji");
+
+  card.appendChild(cityDisplay);
+  card.appendChild(tempDisplay);
+  card.appendChild(humidityDisplay);
+  card.appendChild(descDisplay);
+  card.appendChild(weatherEmoji);
+}
+
+function getWeatherEmoji(weatherId) {
+  switch (true) {
+    case weatherId >= 200 && weatherId < 300:
+      return "⛈️";
+      break;
+    case weatherId >= 300 && weatherId < 400:
+      return "🌧️";
+      break;
+    case weatherId >= 500 && weatherId < 600:
+      return "🌦️";
+      break;
+    case weatherId >= 600 && weatherId < 700:
+      return "❄️";
+      break;
+    case weatherId >= 700 && weatherId < 800:
+      return "🌫️";
+      break;
+    case weatherId === 800:
+      return "☀️";
+      break;
+    case weatherId >= 801 && weatherId < 810:
+      return "⛅";
+      break;
+    default:
+      return "?";
+  }
+}
 
 function displayError(message) {
   const errorDisplay = document.createElement("p");
