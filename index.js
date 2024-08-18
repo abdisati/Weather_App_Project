8e8078134613791652dda878094f57b4b29a5b0e
Abdi Deresa <Abdishegute@gmail.com>
2024-08-17 21:07:21 -0700

    added some js const

---
+++ index.js
@@ -0,0 +1,7 @@
+//Weather App
+
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".cityInput");
+const card = document.querySelector(".card");
+
+const apiKey = "c030ed5eefff362e384926114448ad97";
