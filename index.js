28ef4f63380879dd4a311b9d36258ae8917304f8
Abdi Deresa <Abdishegute@gmail.com>
2024-08-17 21:10:56 -0700

    added functions

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ const cityInput = document.querySelector(".cityInput");
 const card = document.querySelector(".card");
 
 const apiKey = "c030ed5eefff362e384926114448ad97";
+
+weatherForm.addEventListener("sumbit", (event) => {});
+
+async function getWeatherData(city) {}
+
+function displayWeatherInfo(data) {}
+
+function getWeatherEmoji(weatherId) {}
