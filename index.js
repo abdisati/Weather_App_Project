7a998d97d0c7147a22cf546c82bcf8fe426c24c2
Abdi Deresa <Abdishegute@gmail.com>
2024-08-17 21:30:01 -0700

    modified some functions

--- index.js
+++ index.js
@@ -6,10 +6,25 @@ const card = document.querySelector(".card");
 
 const apiKey = "c030ed5eefff362e384926114448ad97";
 
-weatherForm.addEventListener("sumbit", (event) => {});
+weatherForm.addEventListener("sumbit", (event) => {
+  event.preventDefault();
+  const city = cityInput.value;
+  if (city) {
+  } else {
+    displayError("Please enter a city");
+  }
+});
 
 async function getWeatherData(city) {}
 
 function displayWeatherInfo(data) {}
 
 function getWeatherEmoji(weatherId) {}
+
+function displayError(message) {
+  const errorDisplay = document.createElement("p");
+  errorDisplay.textContent = message;
+  errorDisplay.classList.add("errorDisplay");
+
+  card.textContent = "";
+}
